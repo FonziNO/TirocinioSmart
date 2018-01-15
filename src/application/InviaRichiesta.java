33b7cc7a54681f2e2d41acce0512005c9c1d3fa6
FonziNO <fonzino.micky@hotmail.it>
2018-01-15 17:05:46 +0100

    Invio Richiesta
    Modifiche per import della mail dell'azienda nel db

--- src/application/InviaRichiesta.java
+++ src/application/InviaRichiesta.java
@@ -12,27 +12,28 @@ import javax.servlet.http.HttpSession;
 import storage.Richiesta;
 import storage.RichiestaDao;
 
-public class InviaRichiesta extends HttpServlet{
+public class InviaRichiesta extends HttpServlet {
 	RichiestaDao ricDao = new RichiestaDao();
-	Richiesta r=new Richiesta();
-	int res=0;
+	Richiesta r = new Richiesta();
+	int res = 0;
 	private static final long serialVersionUID = 1L;
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		HttpSession session = request.getSession();
 		session.getId();
 		System.out.println("sto per inviare");
 		String emailStudente = (String) session.getAttribute("email");
+
 		System.out.println("Invio la richiesta");
-		try{
-			res=ricDao.richiedi(r.getCounter(), false, emailStudente, "aziendaAFSoluzioni@gmail.it");
-		}catch(SQLException e){
+		try {
+			res = ricDao.richiedi(r.getCounter(), false, emailStudente, request.getParameter("emailAz"));
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-
-
-
-
 }
