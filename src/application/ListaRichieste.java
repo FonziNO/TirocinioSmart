f3dec6fb7228534936b5047a9bdb0209dc922534
FonziNO <fonzino.micky@hotmail.it>
2018-01-20 19:45:56 +0100

    ALFONSO HA FIXATO ANCORA
    Fix dei problemi sull'arraylist + Bestemmie

--- src/application/ListaRichieste.java
+++ src/application/ListaRichieste.java
@@ -12,11 +12,11 @@ import storage.Richiesta;
 import storage.Studente;
 
 public class ListaRichieste {
-	
+
 	public synchronized ArrayList<Richiesta> doListaRichieste() throws SQLException {
 		Connection conn = null;
 		PreparedStatement s1 = null;
-	
+
 		List<Richiesta> listaR = new ArrayList<Richiesta>();
 
 		String listaRichieste = "SELECT * FROM Richiesta INNER JOIN studente ON StudenteEmail = Email";
@@ -29,21 +29,20 @@ public class ListaRichieste {
 		try {
 			while (risultato1.next()) {
 				Richiesta richiesta = new Richiesta();
-				
+
 				richiesta.setIdR(risultato1.getString("ID"));
 				richiesta.setStatoR(risultato1.getBoolean("Stato"));
 				richiesta.setStatoT(risultato1.getBoolean("StatoTutor"));
 				richiesta.setStatoU(risultato1.getBoolean("StatoUfficio"));
 				richiesta.setEmailS(risultato1.getString("StudenteEmail"));
 				richiesta.setEmailA(risultato1.getString("AziendaEmail"));
-				
+
 				richiesta.setNomeS(risultato1.getString("Nome"));
 				richiesta.setCognomeS(risultato1.getString("Cognome"));
 				richiesta.setMatricolaS(risultato1.getString("Matricola"));
-				
+
 				listaR.add(richiesta);
-				
-				
+
 			}
 
 		} catch (Exception e) {
@@ -67,20 +66,19 @@ public class ListaRichieste {
 		}
 		return (ArrayList<Richiesta>) listaR;
 	}
-	
+
 	public synchronized ArrayList<Richiesta> doLista() throws SQLException {
 		Connection conn = null;
 		PreparedStatement s1 = null;
-	
+
 		List<Richiesta> listaR = new ArrayList<Richiesta>();
 
-		//String listaRichieste = "SELECT * FROM Richiesta INNER JOIN studente ON StudenteEmail = Email";
-		String listaRichieste ="select Richiesta.Stato, Richiesta.StudenteEmail, Studente.Nome, Studente.Cognome, Studente.Matricola"+
-		" from Richiesta, Studente "+
-		" where Studente.Email=Richiesta.StudenteEmail "+
-		" group by richiesta.StudenteEmail "+
-		" having richiesta.Stato=false";
+		// String listaRichieste = "SELECT * FROM Richiesta INNER JOIN studente
+		// ON StudenteEmail = Email";
+		String listaRichieste = "select Richiesta.Stato, Richiesta.StudenteEmail, Studente.Nome, Studente.Cognome, Studente.Matricola"
+				+ " from Richiesta, Studente " +
 
+				" group by Richiesta.StudenteEmail " + " having Richiesta.Stato=false";
 
 		conn = DriverManagerConnectionPool.getConnection();
 		s1 = conn.prepareStatement(listaRichieste);
@@ -91,21 +89,20 @@ public class ListaRichieste {
 		try {
 			while (risultato1.next()) {
 				Richiesta richiesta = new Richiesta();
-				
-				//richiesta.setIdR(risultato1.getString("ID"));
+
+				// richiesta.setIdR(risultato1.getString("ID"));
 				richiesta.setStatoR(risultato1.getBoolean("Stato"));
-				//richiesta.setStatoT(risultato1.getBoolean("StatoTutor"));
-				//richiesta.setStatoU(risultato1.getBoolean("StatoUfficio"));
+				// richiesta.setStatoT(risultato1.getBoolean("StatoTutor"));
+				// richiesta.setStatoU(risultato1.getBoolean("StatoUfficio"));
 				richiesta.setEmailS(risultato1.getString("StudenteEmail"));
-				//richiesta.setEmailA(risultato1.getString("AziendaEmail"));
-				
+				// richiesta.setEmailA(risultato1.getString("AziendaEmail"));
+
 				richiesta.setNomeS(risultato1.getString("Nome"));
 				richiesta.setCognomeS(risultato1.getString("Cognome"));
 				richiesta.setMatricolaS(risultato1.getString("Matricola"));
-				
+
 				listaR.add(richiesta);
-				
-				
+
 			}
 
 		} catch (Exception e) {
