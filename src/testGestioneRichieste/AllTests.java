6c4b0da512f27e9f5d331e3dbd1f1a7f573f89ab
AngelaUrsi <angelaursi@virgilio.it>
2018-01-26 10:45:22 +0100

    AllTests

--- src/testGestioneRichieste/AllTests.java
+++ src/testGestioneRichieste/AllTests.java
@@ -4,9 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import application.ListaRichieste;
+
 @RunWith(Suite.class)
-@SuiteClasses({InviaRichiestaTest.class, AccettazioneRichiestaTest.class,  AccettazioneTutorTest.class, ListaAccettatiUfficioTest.class, AccettazioneUfficioTest.class,
-		 ListaRichiesteTest.class, RifiutaRichiestaTest.class })
+@SuiteClasses({InviaRichiestaTest.class, ListaRichiesteTest.class, AccettazioneRichiestaTest.class, ListaAccettatiTutorTest.class, AccettazioneTutorTest.class, ListaAccettatiUfficioTest.class, AccettazioneUfficioTest.class, RifiutaRichiestaTest.class })
 public class AllTests {
 
 }
